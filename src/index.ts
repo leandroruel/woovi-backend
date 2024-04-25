af041be3fc749013234efca0c84d6392adad4452
leandro ruel <proflrr@hotmail.com>
2024-04-25 01:11:27 -0300

    Refactor server.ts and index.ts to use Apollo Server for GraphQL API

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,56 @@
 import app from 'server'
-import process from 'process'
-
+import http from 'http'
+import cors from '@koa/cors'
+import dotenv from 'dotenv'
 import { NODE_PORT } from './config'
 
-let currentApp: any = app
+import { ApolloServer } from '@apollo/server'
+import typeDefs from './graphql/typeDefs'
+import resolvers from './graphql/resolvers'
+import { koaMiddleware } from '@as-integrations/koa'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
 
-// currentApp.shutdown = () => process.exit()
+dotenv.config()
 
-// process.on('SIGINT', () => currentApp.shutdown())
+const EXTERNAL_ENDPOINT = String(process.env.GRAPHQL_URL)
 
-// process.on('SIGTERM', () => currentApp.shutdown())
+async function server({ typeDefs, resolvers }: any) {
+  const httpServer = http.createServer()
 
-currentApp.listen(NODE_PORT, () =>
-  console.log(`Listening on port ${NODE_PORT}`)
-)
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    introspection: true
+  })
+
+  await apolloServer.start()
+
+  httpServer.on('request', app.callback())
+
+  app.use(
+    koaMiddleware(apolloServer, {
+      context: async ({ ctx }) => ({ token: ctx.headers.token })
+    })
+  )
+
+  await new Promise((resolve: any) =>
+    httpServer.listen({ port: NODE_PORT }, resolve)
+  )
+
+  console.log(`🚀 Server ready at ${EXTERNAL_ENDPOINT}`)
+
+  return { apolloServer, app }
+}
+
+server({ typeDefs, resolvers }).then(({ app }) => {
+  app.use(
+    cors({
+      origin: '*',
+      allowMethods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE', 'PATCH'],
+      exposeHeaders: ['X-Request-Id']
+    })
+  )
+})
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,11 @@
 import Koa from 'koa'
 import logger from 'koa-logger'
 import { koaBody } from 'koa-body'
-import helmet from 'koa-helmet'
-import cors from '@koa/cors'
 
-import {
-  getGraphQLParameters,
-  processRequest,
-  renderGraphiQL,
-  shouldRenderGraphiQL,
-  sendResult
-} from 'graphql-helix'
-import Mongoose from 'mongoose'
-import { schema } from './graphql/schema'
+import Mongoose, { ConnectOptions } from 'mongoose'
 import router from './routes'
 import { MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_URL } from './config'
-import { formatGraphQLResult } from '@/helpers/errors'
 import { errorHandlingMiddleware } from '@/middlewares/error-handling-middleware'
-import { validationMiddleware } from '@/middlewares/validation-middleware'
 
 const app = new Koa()
 
@@ -25,60 +13,21 @@ const app = new Koa()
 app.use(logger())
 
 // Database
-const mongooseOptions = {
+const mongooseOptions: ConnectOptions = {
   user: MONGODB_USERNAME,
-  pass: MONGODB_PASSWORD
+  pass: MONGODB_PASSWORD,
+  connectTimeoutMS: 10000,
+  socketTimeoutMS: 45000
 }
 
 Mongoose.connect(MONGODB_URL, mongooseOptions)
 
-// app.use(
-//   helmet({
-//     contentSecurityPolicy: false
-//   })
-// )
+Mongoose.connection.on('error', (error) => {
+  console.log('Error connecting to database', error)
+})
 
 // Body parser
 app.use(koaBody({ multipart: true }))
-
-// GraphQL
-app.use(async (ctx) => {
-  const request: any = {
-    body: ctx.request.body,
-    headers: ctx.req.headers,
-    method: ctx.request.method,
-    query: ctx.request.query
-  }
-
-  console.log('variables input', request.body)
-
-  if (shouldRenderGraphiQL(request)) {
-    ctx.body = renderGraphiQL()
-  } else {
-    const { operationName, query, variables } = getGraphQLParameters(request)
-
-    const result = await processRequest({
-      operationName,
-      query,
-      variables,
-      request,
-      schema
-    })
-
-    ctx.respond = false
-    sendResult(result, ctx.res, formatGraphQLResult)
-  }
-})
-
-app.use(
-  cors({
-    origin: '*',
-    allowMethods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE', 'PATCH'],
-    exposeHeaders: ['X-Request-Id']
-  })
-)
-
-app.use((ctx, next) => validationMiddleware(ctx.request.body.query))
 
 app.use(errorHandlingMiddleware)
 
