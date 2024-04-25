e832373b2e6fffb7af94eac509d38ae951ae8bd0
leandro ruel <proflrr@hotmail.com>
2024-04-25 01:10:40 -0300

    Refactor user-resolver.ts to import UserController from correct file

--- src/graphql/resolvers/user-resolver.ts
+++ src/graphql/resolvers/user-resolver.ts
@@ -1,7 +1,9 @@
 import User from '@/models/User'
-import UserController from '@/controllers/user-controller'
+import UserController from '@/controllers/user.controller'
 
-export const userResolver = {
+import { GraphQLError } from 'graphql'
+
+const userResolver = {
   Query: {
     async users() {
       return await User.find()
@@ -11,9 +13,12 @@ export const userResolver = {
     }
   },
   Mutation: {
-    async createUser(_: any, { input }: any) {
-      UserController.create(input)
-      // return await User.create(input)
+    async createUser(_: any, args: any, ctx: any) {
+      try {
+        return await UserController.create(args)
+      } catch (error: any) {
+        throw new GraphQLError(error)
+      }
     },
     async updateUser(_: any, { id, input }: any) {
       return await User.findByIdAndUpdate(id, input, { new: true })
@@ -21,5 +26,10 @@ export const userResolver = {
     async deleteUser(_: any, { id }: any) {
       return await User.findByIdAndDelete(id)
     }
+  },
+  User: {
+    taxId: (user: InstanceType<typeof User>) => user.tax_id
   }
 }
+
+export default userResolver
