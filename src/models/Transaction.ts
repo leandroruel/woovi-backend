d239e14b067a193fef572c691010cc72bda4036e
leandro ruel <proflrr@hotmail.com>
2024-05-06 00:17:07 -0300

    Add type and state fields to Transaction model in Transaction.ts

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -10,7 +10,18 @@ const transactionSchema = new Schema({
     required: true
   },
   idempotencyId: { type: String, required: true, unique: true },
-  value: { type: Number, required: true }
+  value: { type: Number, required: true },
+  type: {
+    type: String,
+    enum: ['transfer', 'deposit', 'withdraw'],
+    required: true
+  },
+  state: {
+    type: String,
+    enum: ['done', 'pending'],
+    default: 'pending'
+  },
+  createdAt: { type: Date, default: Date.now }
 })
 
 const Transaction = model('Transaction', transactionSchema)
