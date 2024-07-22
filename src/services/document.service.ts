31d6a412a30b4a93d0773cac6aa8133c1b0bb205
leandro ruel <proflrr@hotmail.com>
2024-07-22 17:38:51 -0300

    chore: Refactor document service for improved code organization and error handling

--- src/services/document.service.ts
+++ src/services/document.service.ts
@@ -2,14 +2,14 @@ import {
   CONECTAGOV_API_URL,
   CONECTAGOV_CLIENT_ID,
   CONECTAGOV_CLIENT_SECRET,
-  CONECTAGOV_OAUTH_URL
-} from '@/helpers/document'
-import { extractExp } from '@/helpers/jwt'
-import { isBefore } from 'date-fns'
+  CONECTAGOV_OAUTH_URL,
+} from "@/helpers/document";
+import { extractExp } from "@/helpers/jwt";
+import { isBefore } from "date-fns";
 
-let tokenStorage: { access_token: string } | null = null
+let tokenStorage: { access_token: string } | null = null;
 
-const EXPIRATION_WINDOW_IN_SECONDS = 300
+const EXPIRATION_WINDOW_IN_SECONDS = 300;
 
 /**
  * Autentica a aplicação no ConectaGov
@@ -19,26 +19,24 @@ const EXPIRATION_WINDOW_IN_SECONDS = 300
 const authenticate = async (ctx: any): Promise<any> => {
   try {
     const options = {
-      method: 'POST',
+      method: "POST",
       url: CONECTAGOV_OAUTH_URL,
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization:
-          'Basic ' +
-          Buffer.from(
-            `${CONECTAGOV_CLIENT_ID}:${CONECTAGOV_CLIENT_SECRET}`
-          ).toString('base64')
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${Buffer.from(
+          `${CONECTAGOV_CLIENT_ID}:${CONECTAGOV_CLIENT_SECRET}`,
+        ).toString("base64")}`,
       },
-      body: 'grant_type=client_credentials'
-    }
+      body: "grant_type=client_credentials",
+    };
 
-    const response = await ctx.curl(options.url, options)
+    const response = await ctx.curl(options.url, options);
 
-    return response
+    return response;
   } catch (error: any) {
-    throw new Error(error)
+    throw new Error(error);
   }
-}
+};
 
 /**
  * Trata a requisição de token
@@ -46,13 +44,13 @@ const authenticate = async (ctx: any): Promise<any> => {
  */
 const handleTokenRequest = async (ctx: any) => {
   try {
-    const response = await authenticate(ctx)
-    tokenStorage = JSON.parse(response['body'])
+    const response = await authenticate(ctx);
+    tokenStorage = JSON.parse(response.body);
   } catch (error) {
-    console.error('Erro na obtenção do token:', error)
-    throw new Error('Failed to obtain token')
+    console.error("Erro na obtenção do token:", error);
+    throw new Error("Failed to obtain token");
   }
-}
+};
 
 /**
  * Obtém o token de acesso do local storage ou faz uma nova requisição
@@ -61,11 +59,11 @@ const handleTokenRequest = async (ctx: any) => {
  */
 const getToken = async (ctx: any): Promise<string | undefined> => {
   if (!tokenStorage || isTokenExpired(tokenStorage.access_token)) {
-    await handleTokenRequest(ctx)
+    await handleTokenRequest(ctx);
   }
 
-  return tokenStorage?.access_token
-}
+  return tokenStorage?.access_token;
+};
 
 /**
  *  Verifica se o token está expirado e retorna um booleano
@@ -73,32 +71,32 @@ const getToken = async (ctx: any): Promise<string | undefined> => {
  * @returns {boolean} Retorna um booleano
  */
 const isTokenExpired = (accessToken: string): boolean => {
-  const exp = extractExp(accessToken)
-  const expirationDate = new Date(exp * 1000)
+  const exp = extractExp(accessToken);
+  const expirationDate = new Date(exp * 1000);
   return isBefore(
     expirationDate,
-    Date.now() + EXPIRATION_WINDOW_IN_SECONDS * 1000
-  )
-}
+    Date.now() + EXPIRATION_WINDOW_IN_SECONDS * 1000,
+  );
+};
 
 export const validateDocument = async (ctx: any) => {
-  const token = await getToken(ctx)
+  const token = await getToken(ctx);
 
   const options = {
-    method: 'GET',
+    method: "GET",
     url: `${CONECTAGOV_API_URL}/api-cpf-light/v2/consulta/cpf`,
     headers: {
       Authorization: `Bearer ${token}`,
-      'x-cpf-usuario': ctx.request.body.cpf
+      "x-cpf-usuario": ctx.request.body.cpf,
     },
     params: {
-      document: ctx.params.document
-    }
-  }
+      document: ctx.params.document,
+    },
+  };
 
-  const response = await ctx.curl(options.url, options)
+  const response = await ctx.curl(options.url, options);
 
-  return response
-}
+  return response;
+};
 
-export default { validateDocument }
+export default { validateDocument };
