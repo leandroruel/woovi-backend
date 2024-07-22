f10c0e81721028d5e26a71c6c1868117240624a2
leandro ruel <proflrr@hotmail.com>
2024-07-22 19:22:57 -0300

    chore: Add account validation and error handling to transfer function

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -1,8 +1,16 @@
 import type { MutationTransferAmountArgs } from "@/generated/graphql";
 import { transferAmount } from "@/services/account.service";
+import { validateTransfer } from "@/validators/account";
+import { GraphQLError } from "graphql";
 
-export const transfer = async (data: MutationTransferAmountArgs) => {
-  const result = await transferAmount(data);
+export const transfer = async (args: MutationTransferAmountArgs) => {
+  const { error } = await validateTransfer.validateAsync(args, {
+    abortEarly: false,
+  });
+
+  if (error) throw new GraphQLError(error);
+
+  const result = await transferAmount(args);
 
   return result;
 };
