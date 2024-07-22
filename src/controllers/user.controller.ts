23f5e3ad5ae9bd2eb226aaebd8f4518fc530a826
leandro ruel <proflrr@hotmail.com>
2024-07-22 19:22:37 -0300

    chore: fix indentation

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,28 +1,28 @@
 import type {
-	MutationCreateUserArgs,
-	MutationLoginArgs,
-	MutationUpdateUserArgs,
+  MutationCreateUserArgs,
+  MutationLoginArgs,
+  MutationUpdateUserArgs,
 } from "@/generated/graphql";
 import {
-	DOCUMENT_ALREADY_EXISTS,
-	EMAIL_ALREADY_EXISTS,
+  DOCUMENT_ALREADY_EXISTS,
+  EMAIL_ALREADY_EXISTS,
 } from "@/helpers/constants";
 import { signToken } from "@/helpers/jwt";
 import { encryptPassword } from "@/helpers/password";
 import {
-	createAccount,
-	generateAccountNumber,
+  createAccount,
+  generateAccountNumber,
 } from "@/services/account.service";
 import {
-	createUser,
-	documentExists,
-	emailExists,
-	loginUser,
-	updateUser,
+  createUser,
+  documentExists,
+  emailExists,
+  loginUser,
+  updateUser,
 } from "@/services/user.service";
 import {
-	validateUserCreate,
-	validateUserUpdate,
+  validateUserCreate,
+  validateUserUpdate,
 } from "@/validators/user-schema";
 import { GraphQLError } from "graphql";
 import { v4 as uuidv4 } from "uuid";
@@ -33,37 +33,37 @@ import { v4 as uuidv4 } from "uuid";
  * @returns
  */
 export const create = async (args: MutationCreateUserArgs) => {
-	const { password, ...rest } = args.user;
+  const { password, ...rest } = args.user;
 
-	const { error } = await validateUserCreate.validateAsync(args, {
-		abortEarly: false,
-	});
+  const { error } = await validateUserCreate.validateAsync(args, {
+    abortEarly: false,
+  });
 
-	if (error) throw new GraphQLError(error);
+  if (error) throw new GraphQLError(error);
 
-	if (await emailExists(args.user.email))
-		throw new GraphQLError(EMAIL_ALREADY_EXISTS);
+  if (await emailExists(args.user.email))
+    throw new GraphQLError(EMAIL_ALREADY_EXISTS);
 
-	if (await documentExists(args.user.taxId))
-		throw new GraphQLError(DOCUMENT_ALREADY_EXISTS);
+  if (await documentExists(args.user.taxId))
+    throw new GraphQLError(DOCUMENT_ALREADY_EXISTS);
 
-	const encryptedPassword = String(await encryptPassword(password));
+  const encryptedPassword = String(await encryptPassword(password));
 
-	const user = await createUser({ ...rest, password: encryptedPassword });
+  const user = await createUser({ ...rest, password: encryptedPassword });
 
-	await createAccount({
-		userId: user.id,
-		balance: 100,
-		idempotencyId: uuidv4(),
-		accountNumber: generateAccountNumber(),
-	});
+  await createAccount({
+    userId: user.id,
+    balance: 100,
+    idempotencyId: uuidv4(),
+    accountNumber: generateAccountNumber(),
+  });
 
-	const token = signToken({ userId: user.id, role: user.role });
+  const token = signToken({ userId: user.id, role: user.role });
 
-	return {
-		token,
-		user,
-	};
+  return {
+    token,
+    user,
+  };
 };
 
 /**
@@ -72,9 +72,9 @@ export const create = async (args: MutationCreateUserArgs) => {
  * @returns {Promise<{token: string, user: User}>} - Token and user
  */
 export const login = async (args: MutationLoginArgs) => {
-	const { email, password } = args;
+  const { email, password } = args;
 
-	return await loginUser({ email, password });
+  return await loginUser({ email, password });
 };
 
 /**
@@ -83,38 +83,38 @@ export const login = async (args: MutationLoginArgs) => {
  * @returns {Promise<User>} - Updated user
  */
 export const update = async (args: MutationUpdateUserArgs) => {
-	const {
-		id,
-		user: { password, ...rest },
-	} = args;
+  const {
+    id,
+    user: { password, ...rest },
+  } = args;
 
-	const { error } = await validateUserUpdate.validateAsync(
-		{ user: rest },
-		{
-			abortEarly: false,
-		},
-	);
+  const { error } = await validateUserUpdate.validateAsync(
+    { user: rest },
+    {
+      abortEarly: false,
+    },
+  );
 
-	if (error) throw new GraphQLError(error);
+  if (error) throw new GraphQLError(error);
 
-	if (args.user.email && (await emailExists(args.user.email)))
-		throw new GraphQLError(EMAIL_ALREADY_EXISTS);
+  if (args.user.email && (await emailExists(args.user.email)))
+    throw new GraphQLError(EMAIL_ALREADY_EXISTS);
 
-	if (args.user.taxId && (await documentExists(args.user.taxId)))
-		throw new GraphQLError(DOCUMENT_ALREADY_EXISTS);
+  if (args.user.taxId && (await documentExists(args.user.taxId)))
+    throw new GraphQLError(DOCUMENT_ALREADY_EXISTS);
 
-	if (password) {
-		const encryptedPassword = String(await encryptPassword(password));
+  if (password) {
+    const encryptedPassword = String(await encryptPassword(password));
 
-		const user = await updateUser(id, { ...rest, password: encryptedPassword });
-		return { ...user, password: encryptedPassword };
-	}
+    const user = await updateUser(id, { ...rest, password: encryptedPassword });
+    return { ...user, password: encryptedPassword };
+  }
 
-	const user = await updateUser(id, rest);
+  const user = await updateUser(id, rest);
 
-	if (!user) return null;
+  if (!user) return null;
 
-	return user;
+  return user;
 };
 
 export default { create, update, login };
