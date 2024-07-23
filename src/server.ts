177b118f28cee04e628a6af97ee9bd165137d52f
leandro ruel <proflrr@hotmail.com>
2024-07-23 12:50:33 -0300

    chore: Update server.ts with middleware for request validation

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,37 @@
-import Koa from 'koa'
-import { koaBody } from 'koa-body'
-import logger from 'koa-logger'
+import Koa from "koa";
+import { koaBody } from "koa-body";
+import logger from "koa-logger";
 
-import Mongoose, { ConnectOptions } from 'mongoose'
-import { MONGODB_PASSWORD, MONGODB_URL, MONGODB_USERNAME } from './config'
-import router from './routes'
+import Mongoose, { type ConnectOptions } from "mongoose";
+import { MONGODB_PASSWORD, MONGODB_URL, MONGODB_USERNAME } from "./config";
+import router from "./routes";
+import checkRequest from "./middlewares/test.middleware";
 
-const app = new Koa()
+const app = new Koa();
 
 // Middlewares
-app.use(logger())
+app.use(logger());
+app.use(checkRequest);
 
 // Database
 const mongooseOptions: ConnectOptions = {
   user: MONGODB_USERNAME,
   pass: MONGODB_PASSWORD,
   connectTimeoutMS: 10000,
-  socketTimeoutMS: 45000
-}
+  socketTimeoutMS: 45000,
+};
 
-Mongoose.connect(MONGODB_URL, mongooseOptions)
+Mongoose.connect(MONGODB_URL, mongooseOptions);
 
-Mongoose.connection.on('error', (error) => {
-  console.log('Error connecting to database', error)
-})
+Mongoose.connection.on("error", (error) => {
+  console.log("Error connecting to database", error);
+});
 
 // Body parser
-app.use(koaBody({ multipart: true }))
+app.use(koaBody({ multipart: true }));
 
 // Routes
-app.use(router.routes())
-app.use(router.allowedMethods())
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-export default app
+export default app;
